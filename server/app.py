82836334e0ca53371e53de69a67bb7a87a484b1b
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-01-26 13:20:54 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -12,7 +12,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
-db.init_app(app)
+db.init_app(app)    
 
 api = Api(app)
 
@@ -46,6 +46,20 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        data = request.get_json()
+        for attr in data:
+            setattr (plant, attr, data[attr])
+            db.session.commit()
+            return make_response(jsonify(plant.to_dict()), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response('', 204)
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
